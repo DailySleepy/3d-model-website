369214ff0e1dd9f5e77fe49669a4c6084fa14018
ExileK1G <2791541029@qq.com>
2025-11-11 09:44:50 +0800

    新增POST /api/auth/login 接口

--- backend/src/main/java/com/example/threedmodel/ThreeDApplication.java
+++ backend/src/main/java/com/example/threedmodel/ThreeDApplication.java
@@ -1,10 +1,6 @@
 package com.example.threedmodel;
 
-import dev.langchain4j.community.store.embedding.redis.spring.RedisEmbeddingStoreAutoConfiguration;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 
 public class ThreeDApplication {
 
