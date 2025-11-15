5d1e211bc57bfcad361c67f7119a80f54faa8b80
kurisuww <zhyw15622390356@gmail.com>
2025-11-16 00:35:23 +0800

    Add SpringBootApplication annotation to main class

--- backend/src/main/java/com/example/threedmodel/ThreeDApplication.java
+++ backend/src/main/java/com/example/threedmodel/ThreeDApplication.java
@@ -1,7 +1,9 @@
 package com.example.threedmodel;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class ThreeDApplication {
 
     public static void main(String[] args) {
