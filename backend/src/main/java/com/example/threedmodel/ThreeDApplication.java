7a932caf2dbdae8467847cb8dc5e3ebbf5e3a30c
cantabile666 <3145732816@qq.com>
2025-11-16 18:50:18 +0800

    Update ThreeDApplication.java
    新增mapperscan

--- backend/src/main/java/com/example/threedmodel/ThreeDApplication.java
+++ backend/src/main/java/com/example/threedmodel/ThreeDApplication.java
@@ -1,9 +1,11 @@
 package com.example.threedmodel;
 
 import org.springframework.boot.SpringApplication;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.example.threedmodel.mapper") // 扫描Mapper接口所在包
 public class ThreeDApplication {
 
     public static void main(String[] args) {
