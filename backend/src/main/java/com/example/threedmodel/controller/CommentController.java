60a8335776ab40a9f6de09afb4c41ac5b2b4911e
cantabile666 <3145732816@qq.com>
2025-12-22 16:40:35 +0800

    fix:版本bug，可忽略

--- backend/src/main/java/com/example/threedmodel/controller/CommentController.java
+++ backend/src/main/java/com/example/threedmodel/controller/CommentController.java
@@ -13,7 +13,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/api/comments")
